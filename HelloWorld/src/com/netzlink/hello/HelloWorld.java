eabda869748aad5a8ec523a80434375b304f4607
Frank Hauptmann <hauptmann@netzlink.com>
2014-08-06 11:45:11 +0200

    JUnit

--- HelloWorld/src/com/netzlink/hello/HelloWorld.java
+++ HelloWorld/src/com/netzlink/hello/HelloWorld.java
@@ -1,16 +1,38 @@
 package com.netzlink.hello;
 
 public class HelloWorld {
+	
+	
 
 	public static void main(String[] args) {
+		HelloWorld hello = new HelloWorld();
 		if (args.length > 0) {
-			System.out.println("Hello " + args[0] + "!");
+			hello.setName(args[0]);
+		}
+		hello.sayHello();
+
+	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String sayHello() {
+		String out;
+		if (this.name == null) {
+			out = "Hello World!";
 		}
 		else {
-			System.out.println("Hello World!");
+			out = "Hello " + this.getName() + "!"; 
 		}
-		
-
+		System.out.println(out);
+		return out;
 	}
 
 }
---
+++ HelloWorld/src/tests/HelloWorldTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.netzlink.hello.HelloWorld;
+
+public class HelloWorldTest extends HelloWorld {
+
+	@Test
+	public void testGetName() {
+		HelloWorld hello = new HelloWorld();
+		hello.setName("myName");
+		assertEquals("myName", hello.getName());
+	}
+
+	@Test
+	public void testSetName() {
+		HelloWorld hello = new HelloWorld();
+		hello.setName("myName");
+		assertEquals("myName", hello.getName());
+	}
+
+	@Test
+	public void testSayHello() {
+		HelloWorld hello = new HelloWorld();
+		hello.setName("myName");
+		String out = hello.sayHello();
+		assertEquals("Hello myName!", out);
+	}
+
+}
