e807f4bdc9bceaa66ce8459f3a7b21c608062f13
Frank Hauptmann <hauptmann@netzlink.com>
2014-08-06 11:03:13 +0200

    Kann jetzt Namen

--- HelloWorld/src/com/netzlink/hello/HelloWorld.java
+++ HelloWorld/src/com/netzlink/hello/HelloWorld.java
@@ -3,7 +3,13 @@ package com.netzlink.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		if (args.length > 0) {
+			System.out.println("Hello " + args[0] + "!");
+		}
+		else {
+			System.out.println("Hello World!");
+		}
+		
 
 	}
 
